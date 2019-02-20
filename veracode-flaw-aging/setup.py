302be94e7928500299698eec165a30d682daed45
pmcneil@veracode.com <pmcneil@PMCNEIL-820.Veracode.local>
2019-02-20 11:53:05 -0500

    Fix app name unicode issue and custom field parsing

--- veracode-flaw-aging/setup.py
+++ veracode-flaw-aging/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="veracodeflawaging",
-    version="1.1",
+    version="1.2",
     packages=find_packages(),
     license="MIT",
     author="Patrick McNeil & Chris Campbell",
